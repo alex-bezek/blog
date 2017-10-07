60e35d9c534d99a2c414531e45669c680e8f3ccf
Alex Bezek <alex.bezek@cerner.com>
2017-10-07 16:31:00 -0500

    fix site config

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,7 @@ module.exports = {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "posts",
-        path: `${__dirname}/content/posts`
+        path: `${__dirname}/${config.blogPostDir}`
       }
     },
     `gatsby-transformer-sharp`,
